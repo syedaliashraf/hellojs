b06e4ad32bca73f82d687c2be34fbc39a095ec64
Ali Ashraf <aliashraf@buzzboard.com>
2017-07-27 09:31:31 +0000

    removed default code from app.js and added a simple var

--- app/app.js
+++ app/app.js
@@ -1,12 +1,3 @@
-/*eslint-env node*/
+var name = 'ali'
+console.log(name)
 
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
